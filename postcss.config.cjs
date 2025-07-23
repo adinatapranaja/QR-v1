1dd0345d17d3337b6a1f0b3dc1851f58af72ceed
adinatapranaja <adinataap.pranaja@gmail.com>
2025-07-23 15:04:13 +0700

    simplified commit message for deployment

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,7 +1,4 @@
-// postcss.config.cjs
+// postcss.config.cjs - Completely disable all plugins temporarily
 module.exports = {
-  plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
-  ],
+  plugins: [],
 }
